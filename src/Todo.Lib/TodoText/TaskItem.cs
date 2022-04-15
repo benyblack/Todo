9d6685cc59c8da417ebd56e4c902196c24e9a83d
Behnam <benyblack@gmail.com>
2022-04-15 18:02:02 +0200

    Add TaskItem.Import method

--- src/Todo.Lib/TodoText/TaskItem.cs
+++ src/Todo.Lib/TodoText/TaskItem.cs
@@ -1,14 +1,56 @@
-﻿namespace Todo.Lib.TodoText;
+﻿using System.Text.RegularExpressions;
+
+namespace Todo.Lib.TodoText;
 
 public class TaskItem
 {
-	public bool IsCompleted { get; set; }
+	public bool IsCompleted { get; set; } = false;
 	public string? Priority { get; set; }
 	public DateTime? CompletionDate { get; set; }
-	public DateTime? CreationDate { get; set; }
+	public DateTime? CreateDate { get; set; }
 	public string? Description { get; set; }
 	public string[]? Projects { get; set; }
 	public string[]? Contexts { get; set; }
 	public Dictionary<string, string>? Metadata { get; set; }
 
+	public void Import(string line)
+	{
+		var priorityRegex = new Regex(@"\(([A-Z])\)");
+		var completedRegex = new Regex(@"^x");
+		var creationDateRegex = new Regex(@"\d{4}-\d{2}-\d{2}");
+		var completionDateRegex = new Regex(@"\d{4}-\d{2}-\d{2}");
+		var descriptionRegex = new Regex(@"^x\s.*$");
+		var projectRegex = new Regex(@"\+([^\s]+)");
+		var contextRegex = new Regex(@"@([^\s]+)");
+		var metadataRegex = new Regex(@"(\s+|^)(\S+):(\S+)");
+
+		var completed = completedRegex.Match(line).Value;
+		if (completed == "x")
+		{
+			IsCompleted = true;
+		}
+		this.Priority = priorityRegex.Match(line).Groups[1]?.Value;
+
+		var creationDate = creationDateRegex.Match(line).Value;
+		var completionDate = completionDateRegex.Match(line).Value;
+		this.CompletionDate = !string.IsNullOrEmpty(completionDate) ? DateTime.Parse(completionDate) : null;
+		this.CreateDate = !string.IsNullOrEmpty(creationDate) ? DateTime.Parse(creationDate) : null;
+
+		this.Projects = projectRegex.Matches(line).Select(x => x.Groups[1].Value).ToArray();
+		this.Contexts = contextRegex.Matches(line).Select(x => x.Groups[1].Value).ToArray();
+
+		var metaKeyVals = metadataRegex.Matches(line).Select(x => x.Value).ToArray();
+		this.Metadata = metaKeyVals.Select(x => x.Split(':')).ToDictionary(x => x[0].Trim(), x => x[1].Trim());
+
+		var description = completedRegex.Replace(line, "", 1);
+		description = priorityRegex.Replace(description, "", 1);
+		description = creationDateRegex.Replace(description, "", 1);
+		description = completionDateRegex.Replace(description, "", 1);
+		this.Description = description.Trim();
+	}
+
+	public override string ToString()
+	{
+		throw new NotImplementedException();
+	}
 }
---
+++ tests/Todo.Tests/Todo.Lib.TodoText/TaskItemTests.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using Todo.Lib.TodoText;
+using Xunit;
+
+namespace Todo.Tests.Todo.Lib.TodoText;
+public class TaskItemTests
+{
+
+	[Fact]
+	public void Import_GivenLineWithDescription_ShouldSetDescription()
+	{
+		// Arrange
+		var taskItem = new TaskItem();
+		string completed = "x";
+		string priority = "(A)";
+		string dueDate = "2022-01-01";
+		string createDate = "2022-01-01";
+		string taskDescription = "description of the task +test @home @work +project1 +project2";
+
+		// Act
+		taskItem.Import($"{completed} {priority} {dueDate} {createDate} {taskDescription}");
+
+		// Assert
+		Assert.Equal(taskDescription, taskItem.Description);
+	}
+
+	[Fact]
+	public void Import_GivenLineWithDates_ShouldSetDates()
+	{
+		// Arrange
+		var taskItem = new TaskItem();
+		string completed = "x";
+		string priority = "(A)";
+		string dueDate = "2022-01-01";
+		string createDate = "2022-01-01";
+		string taskDescription = "description of the task +test @home @work +project1 +project2";
+
+		// Act
+		taskItem.Import($"{completed} {priority} {dueDate} {createDate} {taskDescription}");
+
+		// Assert
+		Assert.Equal(DateTime.Parse(dueDate), taskItem.CompletionDate);
+		Assert.Equal(DateTime.Parse(createDate), taskItem.CreateDate);
+	}
+
+	[Fact]
+	public void Import_GivenLineWithPriority_ShouldSetPriority()
+	{
+		// Arrange
+		var taskItem = new TaskItem();
+
+		// Act
+		taskItem.Import("(A) 1 +test @home @work +project1 +project2");
+
+		// Assert
+		Assert.Equal("A", taskItem.Priority);
+	}
+
+	[Fact]
+	public void Import_GivenLineMarkedCompleted_ShouldSetCompleted()
+	{
+		// Arrange
+		var taskItem = new TaskItem();
+
+		// Act
+		taskItem.Import("x 1 +test @home @work +project1 +project2");
+
+		// Assert
+		Assert.True(taskItem.IsCompleted);
+	}
+
+	[Fact]
+	public void Import_GivenLineWithProjects_ShouldSetProjects()
+	{
+		// Arrange
+		var taskItem = new TaskItem();
+
+		// Act
+		taskItem.Import("x 1 +test @home @work +project1 +project2");
+
+		// Assert
+		Assert.Equal(new string[] { "test", "project1", "project2" }, taskItem.Projects);
+	}
+
+	[Fact]
+	public void Import_GivenLineWithContexts_ShouldSetContexts()
+	{
+		// Arrange
+		var taskItem = new TaskItem();
+
+		// Act
+		taskItem.Import("x 1 +test @home @work +project1 +project2");
+
+		// Assert
+		Assert.Equal(new string[] { "home", "work" }, taskItem.Contexts);
+	}
+
+	[Fact]
+	public void Import_GivenLineWithMetadata_ShouldSetMetadata()
+	{
+		// Arrange
+		var taskItem = new TaskItem();
+		var expected = new Dictionary<string, string>() { { "tag1", "val1" }, { "tag2", "val2" } };
+
+
+		// Act
+		taskItem.Import("x 1 +test @home @work tag1:val1 +project1 +project2 tag2:val2");
+
+		// Assert
+		Assert.Equal(expected, taskItem.Metadata);
+	}
+}
