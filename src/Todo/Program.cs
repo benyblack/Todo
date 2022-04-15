6b5b91fc9db9fa3fde3086ee85a1c50f235dc5d6
Behnam <benyblack@gmail.com>
2022-04-16 01:03:17 +0200

    Add simple usage in th app

--- src/Todo/Program.cs
+++ src/Todo/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO.Abstractions;
+using Todo.Lib.TodoText;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hi!");
+var taskItemManager = new TaskItemManager(new FileSystem());
+taskItemManager.AddTask("3 @home2 @work2 +project3 +project4");
+taskItemManager.AddTask("x Salam @home2 @work2 +project3 +project4");
+taskItemManager.AddTask("Hi There @home2 @work2 +project3 +project4");
+var taskItems = taskItemManager.GetAll();
+foreach (var item in taskItems)
+{
+	Console.WriteLine(item.ToString());
+}
+File.Delete("todo.txt");
