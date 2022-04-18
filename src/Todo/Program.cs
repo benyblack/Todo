ea34205de7b3ee7fb9faa14d9091a856ca7f6e89
Behnam <benyblack@gmail.com>
2022-04-19 00:49:35 +0200

    Add some basics to console app

--- src/Todo/Program.cs
+++ src/Todo/Program.cs
@@ -1,78 +1,100 @@
 ﻿using System.IO.Abstractions;
+using Spectre.Console;
 using Todo.Lib.TodoText;
 // See https://aka.ms/new-console-template for more information
 var taskItemManager = new TaskItemManager(new FileSystem());
-Console.WriteLine("Your Todo items:");
 var taskItems = taskItemManager.GetAll();
-for (int i = 0; i < taskItems.Count; i++)
+PrintAllTasks();
+char? input = ' ';
+while (input != 'q')
 {
-	Console.WriteLine($"{i + 1}. {taskItems[i]}");
-}
-Console.WriteLine("==========================================================");
-string? input = "";
-while (input != "quit" && input != "q")
-{
-	Console.WriteLine("What would you like to do? [(a)dd, (l)ist, (r)emove, (e)dit, (q)uit]");
-	input = Console.ReadLine()?.ToLower();
+	//Console.WriteLine("What would you like to do? [(a)dd, (l)ist, (r)emove, (e)dit, (q)uit]");
+	AnsiConsole.MarkupLine("==========================================================");
+	AnsiConsole.MarkupLine("What would you like to do?");
+	AnsiConsole.MarkupLine("([bold]a[/])dd, ([bold]r[/])emove, ([bold]e[/])dit, ([bold]q[/])uit, ([bold]m[/])ark as done/undone?");
+	input = char.ToLower(Console.ReadKey().KeyChar);
+	Console.WriteLine();
 	switch (input)
 	{
-		case "a":
-		case "add":
+		case 'a':
 			AddTask();
 			break;
-		case "l":
-		case "list":
-			PrintAllTasks();
-			break;
-		case "r":
-		case "remove":
+		case 'r':
 			RemoveTask();
 			break;
-		case "e":
-		case "edit":
+		case 'e':
 			EditTask();
 			break;
-		case "q":
-		case "quit":
+		case 'm':
+			MarkTask();
+			break;
+		case 'q':
 			break;
 		default:
 			Console.WriteLine("Invalid input");
 			break;
 	}
 	taskItems = taskItemManager.GetAll();
+	PrintAllTasks();
+}
+
+void MarkTask()
+{
+	var taskNumber = GetSelectedTaskNumber();
+	taskItemManager.Mark(taskNumber);
 }
 
 void AddTask()
 {
-	Console.WriteLine("Enter a description:");
+	AnsiConsole.MarkupLine("Enter a [bold]description[/]:");
 	var description = Console.ReadLine();
 	if (description is { Length: > 0 })
 		taskItemManager.AddTask(description);
+	else
+		AnsiConsole.MarkupLine("[red]Invalid input[/]");
 }
 
 void RemoveTask()
 {
-	Console.WriteLine($"Enter a task number to remove (1 to {taskItems.Count}):");
-	var taskNumber = Console.ReadLine();
-	if (taskNumber is { Length: > 0 })
-		taskItemManager.RemoveTask(int.Parse(taskNumber));
+	var taskNumber = GetSelectedTaskNumber();
+	taskItemManager.RemoveTask(taskNumber);
 }
 
 void EditTask()
 {
-	Console.WriteLine($"Enter a task number to edit (1 to {taskItems.Count}):");
-	var taskNumberToEdit = Console.ReadLine();
+	var taskNumberToEdit = GetSelectedTaskNumber();
 	Console.WriteLine($"Enter a new description:");
 	var newDescription = Console.ReadLine();
-	if (taskNumberToEdit is { Length: > 0 } && newDescription is { Length: > 0 })
-		taskItemManager.EditTask(int.Parse(taskNumberToEdit), newDescription);
+	if (newDescription is { Length: > 0 })
+		taskItemManager.EditTask(taskNumberToEdit, newDescription);
+	else
+		Console.WriteLine("Invalid input");
 }
 
 void PrintAllTasks()
 {
-	var taskItems = taskItemManager.GetAll();
+	Console.Clear();
+	AnsiConsole.MarkupLine("===============================");
+	AnsiConsole.MarkupLine("Task List");
+	AnsiConsole.MarkupLine("===============================");
 	for (int i = 0; i < taskItems.Count; i++)
 	{
-		Console.WriteLine($"{i + 1}. {taskItems[i]}");
+		string tik = taskItems[i].IsCompleted ? "[bold][green]✓[/][/]" : " ";
+		AnsiConsole.MarkupLine($"[[{tik}]] {i + 1}. {taskItems[i].Description}");
 	}
+}
+
+int GetSelectedTaskNumber()
+{
+	return AnsiConsole.Prompt(
+			new TextPrompt<int>($"Enter a task number to edit (1 to {taskItems.Count}):")
+			.PromptStyle("blue")
+			.ValidationErrorMessage($"[red]The input needs to be a number and between 1 to {taskItems.Count}[/]")
+			.Validate(i =>
+			{
+				var max = taskItems.Count;
+				if (i < 1 || i > max)
+					return ValidationResult.Error($"The input needs to be a number and between 1 to {max}");
+				return ValidationResult.Success();
+			}));
 }
